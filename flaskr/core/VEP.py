cc1bbb540691ff7a3a97477935d3975bb6800356
fanyucai1 <fanyucai1@126.com>
2019-12-16 16:03:42 +0800

    no

--- flaskr/core/VEP.py
+++
@@ -1,7 +0,0 @@
-from civicpy import civic, exports
-
-with open('civic_variants.vcf', 'w', newline='') as file:
-        w = exports.VCFWriter(file)
-        all_variants = civic.get_all_variants()
-        w.addrecords(all_variants)
-        w.writerecords()
